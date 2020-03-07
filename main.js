8ad660d9033b6e355b471486ae84ccd9e7ed41c4
raf <rafeedmohtasim@gmail.com>
2020-03-07 12:30:20 -0500

    no idea what i did

--- main.js
+++ main.js
@@ -2,30 +2,29 @@
 var messagesRef = firebase.database().ref("messages");
 
 //listen for form submit
-document.getElementById('contactForm').addEventListener('submit', submitForm);
+document.getElementById("contactForm").addEventListener("submit", submitForm);
 
 //submit form
-function submitForm(e){
-    e.preventDefault();
+function submitForm(e) {
+  e.preventDefault();
 
-    //get values
-    var email = getInputVal('email');
-    var comments = getInputVal('comments');
+  //get values
+  var email = getInputVal("email");
+  var comments = getInputVal("comments");
 
-    saveMessage(email, comments);
+  saveMessage(email, comments);
 }
 
 //function to get form values
-function getInputVal(id){
-    return document.getElementById(id).value;
+function getInputVal(id) {
+  return document.getElementById(id).value;
 }
 
 //save message to firebase
-function saveMessage (email, comments){
-    var newMessageRef = messagesRef.push();
-    newMessageRef.set({
-        email:email,
-        comments:comments
-    })
-
+function saveMessage(email, comments) {
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    email: email,
+    comments: comments
+  });
 }

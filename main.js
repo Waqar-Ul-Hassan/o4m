7cebeaf2d74ae5c812f0df29dcf4137393603c50
rafeedm <rafeedmohtasim@gmail.com>
2020-10-30 18:47:22 -0400

    resolved #11

--- main.js
+++ main.js
@@ -38,19 +38,19 @@ $(document)
 //Vue router
 const fatwa = {
   template:
-    '<div><h3 id="first"><u>Is OSAP permissible</u></h3><br/><zero-md style="text-align: justify;" src="./content/fatwa.md"></zero-md></div>',
+    '<div><h3 id="first"><u>Is OSAP permissible</u></h3><br/><zero-md src="./content/fatwa.md"></zero-md></div>',
 };
 const policies = {
   template:
-    '<div><h3 id="second"><u>Important Policies and Definitions</u></h3><br/><zero-md style="text-align: justify;" src="./content/policies.md"></zero-md></div>',
+    '<div><h3 id="second"><u>Important Policies and Definitions</u></h3><br/><zero-md src="./content/policies.md"></zero-md></div>',
 };
 const advice = {
   template:
-    '<div><h3 id="third"><u>Some OSAP Advice</u></h3><br/><zero-md style="text-align: justify;" src="./content/advice.md"></zero-md></div>',
+    '<div><h3 id="third"><u>Some OSAP Advice</u></h3><br/><zero-md src="./content/advice.md"></zero-md></div>',
 };
 const faq = {
   template:
-    '<div><h3 id="fourth"><u>FAQ</u></h3><br/><zero-md style="text-align: justify;" src="./content/faq.md"></zero-md></div>',
+    '<div><h3 id="fourth"><u>FAQ</u></h3><br/><zero-md src="./content/faq.md"></zero-md></div>',
 };
 
 const routes = [
